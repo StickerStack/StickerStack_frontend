6ee92e31807a423cad9af7215704c91ee7e2ad0b
HanmaDaiki <daikihanma@outlook.com>
2023-02-22 20:28:15 +0300

    feat: add scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,17 @@
 import logo from '../../logo.svg';
 
-import './App.css';
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.App_header}>
+        <img src={logo} className={styles.App_logo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Здесь будет Sticker Stack
         </p>
         <a
-          className="App-link"
+          className={styles.App_link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
