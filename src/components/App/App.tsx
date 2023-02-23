00401eef69da08c6552aff62bbbdd4bae3f4c7c9
HanmaDaiki <daikihanma@outlook.com>
2023-02-23 22:55:31 +0300

    fix: added authorized user header check

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+
 import { Header } from '../Header';
 
 import styles from './App.module.scss';
 
 const App: React.FC = () => {
+  const [isLogged, setIsLogged] = useState<boolean>(false);
+
   return (
     <div className={styles.App}>
-      <Header isLogged={false} />
+      <Header onClickSignin={() => setIsLogged(true)} isLogged={isLogged} />
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,11 @@ import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 interface IProps {
-  isLogged: boolean;
+  isLogged: boolean,
+  onClickSignin: () => void,
 }
 
-const Header: React.FC<IProps> = ({ isLogged }: IProps) => {
+const Header: React.FC<IProps> = ({ isLogged, onClickSignin }: IProps) => {
   return (
     <header className={styles.header}>
       <Link to='/' className={styles.logo}>
@@ -39,7 +40,7 @@ const Header: React.FC<IProps> = ({ isLogged }: IProps) => {
       {isLogged ? (
         <Link to='/' className={styles.profile} />
       ) : (
-        <button className={styles.signin}>Войти</button>
+        <button className={styles.signin} onClick={onClickSignin}>Войти</button>
       )}
     </header>
   );
