360d6c64b059dd24365b43f6ad87b692b59d8539
HanmaDaiki <daikihanma@outlook.com>
2023-02-23 11:41:03 +0300

    fix: moved link to normalize

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App/App';
 
 import './index.scss';
-import './vendor/normalize.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
