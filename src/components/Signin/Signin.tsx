4a8121e84e68568111cb9ac04d5eaa00417647e7
HanmaDaiki <daikihanma@outlook.com>
2023-02-27 20:34:10 +0300

    feat: add Signup

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -1,11 +1,16 @@
+import { useDispatch } from 'react-redux';
+
 import { ButtonSubmit } from '../UI/ButtonSubmit';
 import { CheckBoxForm } from '../UI/CheckBoxForm';
 import { InputForm } from '../UI/InputForm';
 import { TitleForm } from '../UI/TitleForm';
+import { Signup } from '../Signup';
 
 import styles from './Signin.module.scss';
+import { switchForm } from '../../store/formSlice';
 
 const Signin: React.FC = () => {
+  const dispath = useDispatch();
 
   return (
     <form className={styles.signin}>
@@ -22,7 +27,7 @@ const Signin: React.FC = () => {
         <CheckBoxForm label='Запомнить меня' />
       </div>
       <ButtonSubmit>Войти</ButtonSubmit>
-      <span className={styles.link}>Нет аккаунта? <button type='button' className={styles.button}>Зарегистрироваться</button></span>
+      <span className={styles.link}>Нет аккаунта? <button onClick={() => dispath(switchForm(Signup))} type='button' className={styles.button}>Зарегистрироваться</button></span>
     </form>
   );
 };
