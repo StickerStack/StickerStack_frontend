276c5d0f7440f812a15dfcfdcbd743e97f6d242b
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-03-03 14:48:15 +0300

    fix: styles for go back button

--- src/components/ResetPassword/ResetPassword.tsx
+++ src/components/ResetPassword/ResetPassword.tsx
@@ -7,19 +7,19 @@ import { Signin } from '../Signin';
 
 import styles from './ResetPassword.module.scss';
 import { switchForm } from '../../store/formSlice';
-import { Button } from '../UI/Button';
 import { TextForm } from '../UI/TextForm';
 
 const ResetPassword: React.FC = () => {
   const dispatch = useDispatch();
-        // FIXME: Кнопка НАЗАД является кастомной! Убрать когда дизайнер сделает ее!
-  return(
+  return (
     <form className={styles.resetpassword}>
       <TitleForm>Восстановление пароля</TitleForm>
       <InputForm label='E-mail Вашего аккаунта' name='email' placeholder='vashapochta@gmail.com' />
       <TextForm>Мы направим ссылку на Вашу почту для восстановления пароля</TextForm>
       <ButtonSubmit>Восстановить пароль</ButtonSubmit>
-      <Button onClick={() => dispatch(switchForm(Signin))}>Назад</Button>
+      <button className={styles.button} onClick={() => dispatch(switchForm(Signin))}>
+        <span className={styles.button_text}>Вернуться назад</span>
+      </button>
     </form>
   );
 };
