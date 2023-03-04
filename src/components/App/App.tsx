af13a4f8e87a7c039c146dd2b7dcb9d9700b0980
HanmaDaiki <daikihanma@outlook.com>
2023-03-04 14:35:09 +0300

    fix: open popup with last forn, now always signin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { switchForm } from '../../store/formSlice';
 
 import { Header } from '../Header';
 import { Popup } from '../Popup';
+import { Signin } from '../Signin';
 
 import styles from './App.module.scss';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  
   const [isLogged, setIsLogged] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const setClose = () => {
     setIsOpen(false);
+    dispatch(switchForm(Signin));
   };
 
   return (
