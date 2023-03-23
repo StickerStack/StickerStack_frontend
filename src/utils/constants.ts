340b66962262db774b411cee82837716bb733d02
Alexander Merzlikin <daikihanma@outlook.com>
2023-03-23 14:27:38 +0300

    fix: dev server to prod server

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-useless-escape
 const REG_EMAIL = /^(([^<>()[\]\\.,;:\s@\"]+[A-Za-z0-9]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([A-Za-z0-9]+\.)+[a-zA-Z]{2,}))$/;
-const API_URL = 'http://localhost:8000/api'; // https://k1kku.ddns.net/api   http://localhost:8000/api
+const API_URL = 'https://k1kku.ddns.net/api'; // https://k1kku.ddns.net/api   http://localhost:8000/api
 
 export { API_URL, REG_EMAIL };
   
