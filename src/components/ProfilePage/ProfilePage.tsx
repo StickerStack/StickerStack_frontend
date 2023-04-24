1b1c88ec0b3e83b23dd2846c8bb3e28cda2eee75
melnik <Boherave21>
2023-04-24 23:34:06 +0300

    feat: Added profile layout

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -1,27 +1,59 @@
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
-import { ButtonWithText, TitleForm } from '../UI';
+import {ButtonWithText, TitlePage} from '../UI';
+import {IUserState} from '../../interfaces';
+import EmptyAvatarImage from '../../images/empty-avatar.png';
 
-import { useAppDispatch } from '../../hooks/hooks';
-import { IUserState } from '../../interfaces/IUserState';
-import { logOut } from '../../store/userSlice';
 import styles from './ProfilePage.module.scss';
 
 const ProfilePage = () => {
   const email = useSelector((state: { user: IUserState }) => state.user.email);
 
-  const dispatch = useAppDispatch();
-
-  const onLogOut = () => {
-    dispatch(logOut());
-  };
-
   return (
     <main className={styles.profile}>
-      <TitleForm>Почта: {email}</TitleForm>
-      <ButtonWithText theme='transparent' type='button' onClick={() => onLogOut()}>
-        Выйти
-      </ButtonWithText>
+      <TitlePage>Мои данные</TitlePage>
+      <div className={styles.container}>
+        <div className={styles.avatar}>
+          <img
+            className={`${styles.image} ${styles.empty}`}
+            alt='Аватар'
+            src={EmptyAvatarImage}
+          />
+        </div>
+        <div className={styles.profile_data}>
+          <div className={styles.inputs}>
+            <div className={styles.input_container}>
+              <input type='text' className={styles.input} placeholder='Имя'/>
+              <button
+                className={styles.clear_icon}
+                type='button'
+              />
+            </div>
+            <div className={styles.input_container}>
+              <input type='text' className={styles.input} placeholder='Имя'/>
+              <button
+                className={styles.clear_icon}
+                type='button'
+              />
+            </div>
+            <div className={styles.input_container}>
+              <input type='email' value={email} className={styles.input} placeholder='E-mail'/>
+              <button
+                className={styles.clear_icon}
+                type='button'
+              />
+            </div>
+          </div>
+
+          <ButtonWithText
+            className={styles.button}
+            type='button'
+            theme='filled'
+          >
+            Сохранить
+          </ButtonWithText>
+        </div>
+      </div>
     </main>
   );
 };
