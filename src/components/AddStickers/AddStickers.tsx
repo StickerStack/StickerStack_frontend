81e5d0e6e1f7265f58c524e444a1f858579ee56f
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-05-15 11:18:23 +0300

    feat: add pages info

--- src/components/AddStickers/AddStickers.tsx
+++ src/components/AddStickers/AddStickers.tsx
@@ -1,7 +1,8 @@
 import { useAppDispatch } from '../../hooks/hooks';
-import { ButtonCustom, ButtonWithText, TextUnderline, TitlePage } from '../UI';
+import { TextUnderline, TitlePage } from '../UI';
 import { NewSticker } from '../index';
-import { setIsOpen, setPreviewIsOpen } from '../../store/popupSlice';
+import { setPreviewIsOpen } from '../../store/popupSlice';
+import { pages, pagePrice } from '../../utils/constants';
 
 import styles from './AddStickers.module.scss';
 
@@ -12,7 +13,27 @@ const AddStickers: React.FC = () => {
     <div className={styles.container}>
       <TitlePage>Заказать стикеры</TitlePage>
       <NewSticker />
-      <TextUnderline type='button' onClick={() => dispatch(setPreviewIsOpen(true))}>
+      <div className={styles.info}>
+        <div className={styles.flex}>
+          <span className={styles.text}>Количество листов А4</span>
+          <span className={styles.amount}>{pages.length}</span>
+        </div>
+        <div className={styles.flex}>
+          <span className={styles.text}>Стоимость</span>
+          <div className={styles.prices}>
+            <span className={styles.price}>{pagePrice * pages.length} ₽</span>
+            <span className={styles.price_small}>
+              {(pagePrice * pages.length) / (pages.length * 10)}₽/ за шт
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <TextUnderline
+        type='button'
+        className={styles.preview}
+        onClick={() => dispatch(setPreviewIsOpen(true))}
+      >
         Предпросмотр страницы
       </TextUnderline>
     </div>
--- src/components/UI/TextUnderline/TextUnderline.tsx
+++ src/components/UI/TextUnderline/TextUnderline.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { Link } from 'react-router-dom';
 import styles from './TextUnderline.module.scss';
 
@@ -5,14 +6,20 @@ export type TextType = 'button' | 'link';
 
 interface IProps {
   children: React.ReactNode;
+  className?: string;
   type?: TextType;
   onClick?: () => void;
 }
 
-const TextUnderline: React.FC<IProps> = ({ children, type, onClick }: IProps) => {
+const TextUnderline: React.FC<IProps> = ({
+  children,
+  className,
+  type = 'button',
+  onClick,
+}: IProps) => {
   return type === 'button' ? (
     <button type='button' onClick={onClick} className={styles.button}>
-      <span className={styles.text}>{children}</span>
+      <span className={cn(styles.text, className)}>{children}</span>
     </button>
   ) : type === 'link' ? (
     // TODO: добработать ссылку, когда появится
