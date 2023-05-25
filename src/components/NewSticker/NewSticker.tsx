d9a6511aae93ad3f6b8b81a8386e3e91a4a6826f
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-05-25 14:42:48 +0300

    feat: add validation

--- src/components/NewSticker/NewSticker.tsx
+++ src/components/NewSticker/NewSticker.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import cn from 'classnames';
-import { ButtonCustom, RadioButton, TooltipCustom } from '../UI';
+import { FieldValues, useForm } from 'react-hook-form';
+
+import { ButtonCustom, CheckBoxForm, RadioButton, TooltipCustom } from '../UI';
 import { DragAndDrop } from '../';
 
 import { ReactComponent as RectSvg } from '../../images/icons/rect.svg';
@@ -13,6 +15,14 @@ import styles from './NewSticker.module.scss';
 
 const NewSticker: React.FC = () => {
   const [customVisible, setCustomVisible] = useState<boolean>(false);
+
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm<FieldValues>({
+    mode: 'onBlur',
+  });
 
   return (
     <div className={styles.card}>
@@ -51,8 +61,30 @@ const NewSticker: React.FC = () => {
           </div>
         </fieldset>
         <div className={styles.flex}>
-          <p className={styles.category}>Количество стикеров</p>
-          <input className={cn(styles.input, styles.quantity_input)} />
+          <p className={styles.category} onClick={() => console.log(errors)}>
+            Количество стикеров
+          </p>
+          <input
+            type='tel'
+            className={cn(styles.input, styles.quantity_input)}
+            {...register('amount', {
+              value: 1,
+              required: 'Введите количество стикеров',
+              min: {
+                value: 1,
+                message: 'Минимальное количество - 1',
+              },
+              max: {
+                value: 100,
+                message: 'Максимальное количество - 100',
+              },
+              pattern: {
+                value: /^[1-9][0-9]?$|^100$/,
+                message: 'Укажите корректное количество',
+              },
+            })}
+          />
+          <span className={styles.error}>{errors.amount && `${errors.amount?.message}`}</span>
         </div>
         <fieldset className={styles.flex}>
           <p className={styles.category}>Размер</p>
@@ -61,12 +93,59 @@ const NewSticker: React.FC = () => {
               Оптимальный размер
               <TooltipCustom text={tooltipText} />
             </RadioButton>
-            <RadioButton name='size' value='custom' onClick={() => setCustomVisible(true)}>
+            <RadioButton
+              name='size'
+              value='custom'
+              className={styles.size}
+              onClick={() => setCustomVisible(true)}
+            >
               Свой размер
               <div className={cn(customVisible ? styles.visible : styles.hidden)}>
-                <input className={cn(styles.input, styles.size_input)} placeholder='ширина' /> x{' '}
-                <input className={cn(styles.input, styles.size_input)} placeholder='высота' />
+                <input
+                  placeholder='ширина'
+                  type='tel'
+                  className={cn(styles.input, styles.size_input)}
+                  {...register('width', {
+                    required: 'Введите размеры',
+                    min: {
+                      value: 1,
+                      message: 'Минимальный размер - 1 см',
+                    },
+                    max: {
+                      value: 100,
+                      message: 'Максимальный размер - 25 см',
+                    },
+                    pattern: {
+                      value: /^[1-9][0-9]?$|^100$/,
+                      message: 'Укажите корректные размеры',
+                    },
+                  })}
+                />{' '}
+                x{' '}
+                <input
+                  className={cn(styles.input, styles.size_input)}
+                  placeholder='высота'
+                  type='tel'
+                  {...register('height', {
+                    required: 'Введите размеры',
+                    min: {
+                      value: 1,
+                      message: 'Минимальный размер - 1 см',
+                    },
+                    max: {
+                      value: 100,
+                      message: 'Максимальный размер - 25 см',
+                    },
+                    pattern: {
+                      value: /^[1-9][0-9]?$|^100$/,
+                      message: 'Укажите корректные размеры',
+                    },
+                  })}
+                />
                 <span className={cn(customVisible ? styles.visible : styles.hidden)}> см</span>
+                <span className={styles.error}>
+                  {(errors.width || errors.height) && `${errors.width?.message}`}
+                </span>
               </div>
             </RadioButton>
           </div>
@@ -75,7 +154,12 @@ const NewSticker: React.FC = () => {
           <p className={styles.category}>Цвет фона</p>
           <label className={styles.text}>
             белый
-            <input type='checkbox' />
+            <CheckBoxForm
+              name='colorCheckbox'
+              register={register}
+              option={{ required: 'Обязательное поле' }}
+              error={errors?.confirmCheckbox}
+            />
           </label>
         </div>
         <div className={styles.flex}>
