ee1fa598bb0bad34e30bd8a23aab9a2929135d3d
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-07-31 18:48:05 +0300

    fix: update grid

--- src/components/StickerList/StickerList.tsx
+++ src/components/StickerList/StickerList.tsx
@@ -11,16 +11,16 @@ const StickerList: React.FC = () => {
   const cards = useSelector((state: { cards: ICardsState }) => state.cards.cards);
 
   const pageSizePx = {
-    widthPage: converter.mmToPx(pageSize.widthPage),
-    heightPage: converter.mmToPx(pageSize.heightPage),
+    widthPage: converter.mmToPx(pageSize.widthPage) / 2,
+    heightPage: converter.mmToPx(pageSize.heightPage) / 2,
     paddingList: {
-      top: converter.mmToPx(pageSize.paddingList.top),
-      right: converter.mmToPx(pageSize.paddingList.right),
-      bottom: converter.mmToPx(pageSize.paddingList.bottom),
-      left: converter.mmToPx(pageSize.paddingList.left),
+      top: converter.mmToPx(pageSize.paddingList.top) / 2,
+      right: converter.mmToPx(pageSize.paddingList.right) / 2,
+      bottom: converter.mmToPx(pageSize.paddingList.bottom) / 2,
+      left: converter.mmToPx(pageSize.paddingList.left) / 2,
     },
-    gapX: converter.mmToPx(pageSize.gapX),
-    gapY: converter.mmToPx(pageSize.gapY),
+    gapX: converter.mmToPx(pageSize.gapX) / 2,
+    gapY: converter.mmToPx(pageSize.gapY) / 2,
   };
 
   const borderInPx = converter.mmToPx(stickerWhiteBorder);
@@ -29,52 +29,33 @@ const StickerList: React.FC = () => {
     <div
       className={styles.container}
       style={{
-        width: pageSizePx.widthPage / 2,
-        height: pageSizePx.heightPage / 2,
-        padding: `${pageSize.paddingList.top / 2}px ${pageSize.paddingList.right / 2}px ${
-          pageSize.paddingList.bottom / 2
-        }px ${pageSize.paddingList.left / 2}px`,
-        gridTemplateColumns: `repeat(${Math.floor(pageSizePx.widthPage / 2 / 10)}, 10px)`,
-        gridTemplateRows: `repeat(${Math.floor(pageSizePx.heightPage / 2 / 10)}, 10px)`,
+        width: pageSizePx.widthPage,
+        height: pageSizePx.heightPage,
+        padding: `${pageSize.paddingList.top}px ${pageSize.paddingList.right}px ${pageSize.paddingList.bottom}px ${pageSize.paddingList.left}px`,
+        gridTemplateColumns: `repeat(${Math.floor(pageSizePx.widthPage)}, 1px)`,
+        gridTemplateRows: `repeat(${Math.floor(pageSizePx.heightPage)}, 1px)`,
       }}
     >
       {cards.map((card) => {
-        if (card.amount > 1) {
-          const elements: JSX.Element[] = [];
-          for (let i = 1; i <= card.amount; i++) {
-            elements.push(
-              <div
-                className={cn(styles.border, styles[`border_${card.shape}`])}
-                style={{
-                  width: card.size.width / 2,
-                  height: card.size.height / 2,
-                  padding: borderInPx / 2,
-                  gridRow: `span ${Math.ceil(card.size.height / 2 / 10) + 1}`,
-                  gridColumn: `span ${Math.ceil(card.size.width / 2 / 10) + 1}`,
-                }}
-                key={card.id + i}
-              >
-                <img className={cn(styles.image, styles[`image_${card.shape}`])} src={card.image} />
-              </div>,
-            );
-          }
-          return elements;
-        } else
-          return (
+        const elements: JSX.Element[] = [];
+        for (let i = 1; i <= card.amount; i++) {
+          elements.push(
             <div
               className={cn(styles.border, styles[`border_${card.shape}`])}
               style={{
                 width: card.size.width / 2,
                 height: card.size.height / 2,
                 padding: borderInPx / 2,
-                gridRow: `span ${Math.ceil(card.size.height / 2 / 10) + 1}`,
-                gridColumn: `span ${Math.ceil(card.size.width / 2 / 10) + 1}`,
+                gridRow: `span ${Math.ceil(card.size.height / 2 + pageSizePx.gapY)}`,
+                gridColumn: `span ${Math.ceil(card.size.width / 2 + pageSizePx.gapX)}`,
               }}
-              key={card.id}
+              key={card.id + i}
             >
               <img className={cn(styles.image, styles[`image_${card.shape}`])} src={card.image} />
-            </div>
+            </div>,
           );
+        }
+        return elements;
       })}
     </div>
   );
