5b52fa0b3c215a125c29552c2f7f1e6e417cc2b3
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-08-01 21:26:44 +0300

    feat: add redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import localStorage from 'redux-persist/lib/storage';
 import { popupSliceReducer } from './popupSlice';
 import { userSliceReducer } from './userSlice';
 import { authSliceReducer } from './authSlice';
@@ -11,8 +13,15 @@ const rootReducer = combineReducers({
   cards: cardsSliceReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage: localStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   /* #TODO: Разобраться serializableCheck с включенным падают ошибки!*/
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
@@ -20,6 +29,8 @@ const store = configureStore({
     }),
 });
 
+const persistor = persistStore(store);
+
 const AppDispatch = store.dispatch;
 
-export { store, AppDispatch };
+export { store, persistor, AppDispatch };
