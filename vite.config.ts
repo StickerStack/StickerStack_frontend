04491f40cc67535650c8f8cdd870e57405005457
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-09-30 19:58:32 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
