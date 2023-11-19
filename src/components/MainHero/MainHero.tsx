841e16a30338d6418cdfcc08c2af9bf7733bc94d
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-11-19 12:14:43 +0300

    fix

--- src/components/MainHero/MainHero.tsx
+++ src/components/MainHero/MainHero.tsx
@@ -13,11 +13,12 @@ import { hero } from '@static/mainpage';
 // import ufo from '../../images/main-page/ufo.png';
 // import no_but from '../../images/main-page/no-but-yes.png';
 // import astronaut from '../../images/main-page/astronaut.png';
-import space from '@images/main-page/space-sticker.png';
-import spok from '@images/main-page/spok.png';
+import space from '@images/main-page/space-sticker-square.png';
+import spok from '@images/main-page/spok-square.png';
 import starwars from '@images/main-page/starwars.png';
 import cat from '@images/main-page/space-cat.png';
 import sticker_ufo from '@images/main-page/sticker-ufo-square.png';
+import StarSvg from '@images/main-page/star-decor.svg?react'
 import styles from './MainHero.module.scss';
 
 const MainHero: React.FC = () => {
@@ -44,6 +45,7 @@ const MainHero: React.FC = () => {
       </section>
       <AnimatePresence>
         <motion.div className={styles.images}>
+          
           <motion.img
             src={space}
             className={styles.image}
@@ -115,15 +117,88 @@ const MainHero: React.FC = () => {
             }}
             animate={{
               transition: {
-                opacity: { duration: 0.35, delay: 0.75 },
+                opacity: { duration: 0.35, delay: 0.8 },
                 translateY: { duration: 0.7, delay: 0.6 },
               },
               opacity: 1,
               translateY: 0,
             }}
           />
-          
-          
+          <motion.div
+            className={styles.star}
+            initial={{
+              opacity: 0,
+              translateY: 170,
+            }}
+            animate={{
+              transition: {
+                opacity: { duration: 2 },
+              },
+              opacity: [0, 0.2, 1, 0.65, 1, 0.75, 1],
+              translateY: 0,
+            }}>
+            <StarSvg />
+          </motion.div>
+          <motion.div
+            className={styles.star}
+            initial={{
+              opacity: 0,
+              translateY: 170,
+            }}
+            animate={{
+              transition: {
+                opacity: { duration: 2 },
+              },
+              opacity: [0, 0.2, 1, 0.65, 1, 0.75, 1],
+              translateY: 0,
+            }}>
+            <StarSvg />
+          </motion.div>
+          <motion.div
+            className={styles.star}
+            initial={{
+              opacity: 0,
+              translateY: 170,
+            }}
+            animate={{
+              transition: {
+                opacity: { duration: 2 },
+              },
+              opacity: [0, 0.2, 1, 0.65, 1, 0.75, 1],
+              translateY: 0,
+            }}>
+            <StarSvg />
+          </motion.div>
+          <motion.div
+            className={styles.star}
+            initial={{
+              opacity: 0,
+              translateY: 170,
+            }}
+            animate={{
+              transition: {
+                opacity: { duration: 2 },
+              },
+              opacity: [0, 0.2, 1, 0.35, 1, 0.45, 1],
+              translateY: 0,
+            }}>
+            <StarSvg />
+          </motion.div>
+          <motion.div
+            className={styles.star}
+            initial={{
+              opacity: 0,
+              translateY: 170,
+            }}
+            animate={{
+              transition: {
+                opacity: { duration: 2 },
+              },
+              opacity: [0, 0.2, 1, 0.65, 1, 0.75, 1],
+              translateY: 0,
+            }}>
+            <StarSvg />
+          </motion.div>
         </motion.div>
       </AnimatePresence>
     </Container>
